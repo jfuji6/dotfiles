33729ee47595677e19ce9f13a09114a60c4309c1
jfujitani <jfuji6@gmail.com>
2024-01-04 01:15:29 -0800

    lua-line time format

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    sections = {
+      lualine_z = {
+        function()
+          return " " .. os.date("%I:%M")
+        end,
+      },
+    },
+  },
+}
