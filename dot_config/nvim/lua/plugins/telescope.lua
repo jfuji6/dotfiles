7e554a3e9034f34d01413b494f2e98c3e65c347e
jfujitani <jfuji6@gmail.com>
2024-01-04 18:51:18 -0800

    telescope hidden

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -25,6 +25,18 @@ return {
         sorting_strategy = "ascending",
         winblend = 0,
       },
+      pickers = {
+        live_grep = {
+          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+          additional_args = function(_)
+            return { "--hidden" }
+          end,
+        },
+        find_files = {
+          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+          hidden = true,
+        },
+      },
     },
   },
   {
