13dfb745377830484b095abc26c4be20a38a0436
jfujitani <jfuji6@gmail.com>
2024-01-06 02:18:39 -0800

    ignore bind and obj folder

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -27,13 +27,13 @@ return {
       },
       pickers = {
         live_grep = {
-          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+          file_ignore_patterns = { "bin", "obj", "node_modules", ".git", ".venv" },
           additional_args = function(_)
             return { "--hidden" }
           end,
         },
         find_files = {
-          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+          file_ignore_patterns = { "bin", "obj", "node_modules", ".git", ".venv" },
           hidden = true,
         },
       },
