e48a9e7d5ef19f519e4d29517e367ed075bc5bcc
John Fujitani <jfuji6@gmail.com>
2024-10-01 23:36:17 -0700

    nvim update

--- dot_config/nvim/lua/plugins/disable.lua
+++ dot_config/nvim/lua/plugins/disable.lua
@@ -2,4 +2,5 @@ return {
   { "echasnovski/mini.surround", enabled = false },
   { "echasnovski/mini.pairs", enabled = true },
   { "folke/flash.nvim", enabled = false },
+  { "garymjr/nvim-snippets", enabled = false },
 }
